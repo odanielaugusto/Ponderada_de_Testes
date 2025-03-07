ca26b8d8e6a10130232c23a80c66b9d1bf3dd201
thiagomes07 <thiagogomesalmeida27@gmail.com>
2025-03-07 18:27:19 -0300

    feat: estrutura os diretórios do frontend

---
+++ src/frontend/src/app/pages/professionals/professional-details/professional-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-details',
+  imports: [],
+  templateUrl: './professional-details.component.html',
+  styleUrl: './professional-details.component.scss'
+})
+export class ProfessionalDetailsComponent {
+
+}
