d108d4aaea61de533637e4ff879d39d1e172b4a2
João <joovictorsouza5@gmail.com>
2025-03-10 17:54:21 -0300

    fix: corrigir erros de mesclagem

---
+++ src/frontend/src/app/shared/components/filter/filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-filter',
+    imports: [RouterOutlet],
+    templateUrl: './filter.component.html',
+    styleUrl: './filter.component.scss'
+})
+export class FilterComponent {
+}
