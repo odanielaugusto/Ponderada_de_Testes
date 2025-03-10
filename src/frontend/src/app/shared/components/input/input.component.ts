a04108e59c6a4a7a98882e4b4c9df346b8f2c1d8
annariciopo <anna.riciopo@sou.inteli.edu.br>
2025-03-10 17:41:33 -0300

    feat: adiciona componente de input

--- src/frontend/src/app/shared/components/input/input.component.ts
+++ src/frontend/src/app/shared/components/input/input.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
 export class InputComponent {
-
+  @Input() label!: string;
+  @Input() placeHolder: string = "";
+  @Input() type: string = "text";
+  @Input() width!: string;
+  @Input() height!: string;
 }
