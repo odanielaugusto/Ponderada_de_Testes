3635111cec14c0b3463255b077b8e806c1380000
nataly-cunha-inteli <158575982+nataly-cunha-inteli@users.noreply.github.com>
2025-03-09 22:50:35 -0300

    feat: implementa a autenticação de usuário por auth0
    Este commit implementa o código base necessário para integrar o projeto ao recurso Auth0 que faz a autenticação de usuários, como cumprimento da task: https://trello.com/c/ivWa2myx

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -3,10 +3,27 @@
 
 import express from 'express';
 import userRoutes from './routes/userRoutes';
-import { UserRepository } from './repositories/UserRepository';
+import { UserRepository } from './repositories/userRepository';
+import { Request, Response } from "express";
+import { auth, ConfigParams } from "express-openid-connect";
+import dotenv from "dotenv";
+
+dotenv.config(); // Carrega variáveis de ambiente do .env
 
 const app = express();
 const port = 3000;
+
+const config: ConfigParams = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUERBASEURL,
+  secret: process.env.SECRET
+};
+
+// Middleware do Auth0 para autenticação
+app.use(auth(config));
 
 // Middleware para parser JSON
 app.use(express.json());
@@ -15,10 +32,19 @@ app.use(express.json());
 const userRepository = new UserRepository();
 userRepository.connect();
 
+// Rota protegida para verificar se o usuário está autenticado
+app.get("/", (req: any, res: any) => {
+  if (!req.oidc || !req.oidc.isAuthenticated()) {
+    return res.send("Logged out");
+  }
+  res.send("Logged in");
+});
+
 // Usar as rotas configuradas
 app.use('/api', userRoutes);
 
-// Iniciar o servidor
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+// Inicialização do servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
