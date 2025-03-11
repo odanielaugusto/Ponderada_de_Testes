2996f9d8a5b52c8bade47e3734e0a9fd62b7b225
thiagomes07 <thiagogomesalmeida27@gmail.com>
2025-03-11 11:16:53 -0300

    feat: cria boilerplates de exemplo para cada camada da api

---
+++ src/backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API rodando!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
