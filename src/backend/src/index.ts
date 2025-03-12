66b1f5c1c769c576fd3a035e70ca209dcf4508b7
nataly-cunha-inteli <158575982+nataly-cunha-inteli@users.noreply.github.com>
2025-03-12 15:59:53 -0300

    feat: conserta autenticação de login a partir de mudanças da estrutura-base do projeto
    Este commit, quando aplicado, conserta a autenticação de login com o Auth0 a partir de mudanças da estrutura-base do projeto, puxadas da develop, em cumprimento com a task https://trello.com/c/ivWa2myx

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -6,13 +6,28 @@ import userRoutes from "./routes/userRoutes.js";
 import appointmentRoutes from "./routes/appointmentRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 import professionalRoutes from "./routes/professionalRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import { auth, ConfigParams } from "express-openid-connect";
 
 dotenv.config();
 
 const app = express();  
 app.use(express.json());
 
+const config: ConfigParams = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  secret: process.env.SECRET
+};
+
+// Middleware do Auth0 para autenticação
+app.use(auth(config));
+
 // Usar as rotas configuradas
+app.use("/", authRoutes); 
 app.use("/api/user/", userRoutes);
 app.use("/api/appointment/", appointmentRoutes);
 app.use("/api/dashboard/", dashboardRoutes);
