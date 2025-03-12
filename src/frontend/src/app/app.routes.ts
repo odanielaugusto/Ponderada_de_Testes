be2817bdee10cb868c81c71b66573bbab016cd30
Lucas Brasil <lucas.brasil@sou.inteli.edu.br>
2025-03-12 17:47:33 -0300

    feat(routes): adicionar rotas para home, professionals, student e users
    - Atualizei o arquivo de rotas (app.routes.ts) para incluir novas rotas utilizando lazy loading:
      - Rota 'home' para carregar o módulo da Home (src/app/pages/home/)
      - Rota 'professionals' para carregar o módulo de Professionals (src/app/pages/professionals/)
      - Rota 'student' para carregar o módulo de Student (src/app/pages/student/)
      - Rota 'users' para carregar o módulo de Users (src/app/pages/users/)
    - Configuração do redirecionamento padrão (rota raiz e wildcard) para a página de home.
    - As alterações garantem uma estrutura modular e melhor performance na navegação.

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+    {path: 'professional', loadChildren: () => import('./pages/professionals/professionals.module').then(m => m.ProfessionalsModule)},
+    {path: 'students', loadChildren: () => import('./pages/students/students.module').then(m => m.StudentsModule)},
+    {path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule)}
+];
