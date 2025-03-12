dcd9c29daedf00709b077a49ae556132be13ad26
thiagomes07 <thiagogomesalmeida27@gmail.com>
2025-03-12 17:42:42 -0300

    chore: limpa o conteúdo do hmtml raíz

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,23 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FilterComponent } from './shared/components/filter/filter.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true, // Adicionado standalone: true
-  imports: [RouterOutlet, FilterComponent, CommonModule],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'gallaudet';
-
-  unidades: string[] = ['ETEC Teste 1', 'ETEC Teste 2', 'ETEC Teste 3', 'ETEC Teste 4', 'ETEC Teste 5'];
-  unidadeSelecionada: string = '';
-
-  onUnidadeSelected(unidade: string): void {
-    this.unidadeSelecionada = unidade;
-    console.log('Unidade selecionada:', unidade);
-  }
-}
+export class AppComponent {}
