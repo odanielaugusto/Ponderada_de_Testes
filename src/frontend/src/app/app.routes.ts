aa938b934dc4c5a92c7c2866c82b740dbcd39b4c
Lucas Brasil <lucas.brasil@sou.inteli.edu.br>
2025-03-13 17:43:33 -0300

    fix: corrigi as rotas de entrar e not found

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found/not-found.component';
 
 export const routes: Routes = [
     {path: 'inicio', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
     {path: 'profissional', loadChildren: () => import('./pages/professionals/professionals.module').then(m => m.ProfessionalsModule)},
     {path: 'estudantes', loadChildren: () => import('./pages/students/students.module').then(m => m.StudentsModule)},
     {path: 'usuarios', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule)},
-    { path: '**', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) },
     { path: 'entrar', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)},
-    { path: '', redirectTo: 'entrar', pathMatch: 'full'},
+    { path: '', redirectTo: '/entrar', pathMatch: 'full'},
+    { path: '**', component: NotFoundComponent },
 ];
 
--- src/frontend/src/app/pages/not-found/not-found-routing.module.ts
+++ src/frontend/src/app/pages/not-found/not-found-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [
-  {path: '', component: NotFoundComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
